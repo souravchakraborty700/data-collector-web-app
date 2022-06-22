258a324336227f5bbfae31b548fd4313fb492168
Sourav Chakraborty <souravchakraborty700@gmail.com>
2022-06-22 23:34:04 +0530

    Updated

--- app.py
+++ app.py
@@ -1,8 +1,22 @@
 from crypt import methods
+
 from flask import Flask, render_template, request
-from flask.ext.sqlalchemy import SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI']='postgresql://postgres:sadama123@localhost/height_collector'
+db = SQLAlchemy(app)
+
+class Data(db.Model):
+    __tablename__="data"
+    id = db.Column(db.Integer, primary_key=True)
+    email_ = db.Column(db.String(120),unique= True)
+    height_ = db.Column(db.Integer)
+
+    def __init__(self, email_,height_):
+        self.email_ = email_
+        self.height_ = height_
+
 
 @app.route("/")
 def index():
@@ -13,6 +27,11 @@ def success():
     if request.method == "POST":
         email = request.form["email_name"]
         height = request.form["height_name"]
+        
+        data = Data(email, height)
+        print(db.session.query(Data).filter(Data.email_==email).count()) 
+        db.session.add(data)
+        db.session.commit()
         print(email, height)
         return render_template("success.html")
 
